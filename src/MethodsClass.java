64733a3a1a0602b14fa8166b7c6587b9107bc32b
Eric Abell <ericabell@users.noreply.github.com>
2017-09-20 13:52:32 -0500

    containsOne now throws an exception if input is negative

--- src/MethodsClass.java
+++ src/MethodsClass.java
@@ -1,5 +1,9 @@
 public class MethodsClass {
-    public static boolean containsOne (int n) {
+    public static boolean containsOne (int n) throws BadInputException {
+        if (n < 0) {
+            throw new BadInputException();
+        }
+
         String numString = "" + n; //Convert the number to a string
         return numString.contains("1"); //Check to see if it contains 1
     }
